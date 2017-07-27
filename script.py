59ecfb2325251295134b0c70ded22f6f76c50bc9
Zack P <zack@zacklearns.com>
2017-07-26 19:27:42 -0600

    Create script.py

---
+++ script.py
@@ -0,0 +1,29 @@
+from pyparsing import *
+
+yabbit = open("build.y")
+
+TREE = Keyword("tree")
+NAME = Keyword("name")
+AUTHOR = Keyword("author")
+
+LBRACE,RBRACE,LPAREN,RPAREN,SEMI,EQUAL,COL = map(Suppress,"{}();=:")
+
+real = Regex(r"[+-]?\d+\.\d*").setParseAction(lambda t:float(t[0]))
+integer = Regex(r"[+-]?\d+").setParseAction(lambda t:int(t[0]))
+
+string = QuotedString('"')
+
+# example: tree:'master'{
+#   name:'Yabbit';
+#   author:'Zack Pace';
+# };
+tree = Forward()
+tree << Group(TREE + COL + string("tree") + LBRACE + Group(ZeroOrMore(name | author))("body") + RBRACE)
+
+# example: name:'Yabbit';
+name = Group(NAME + COL + string("name") + SEMI)
+
+# example: author:'Zack Pace';
+author = Group(AUTHOR + COL + string("author") + SEMI)
+
+#
