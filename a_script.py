f6817536cf586d1154cd2fa9eba9e9b5580ca743
Zack P <zack@zacklearns.com>
2017-07-28 08:57:32 -0600

    Update a_script.py

--- a_script.py
+++ a_script.py
@@ -1,11 +1,11 @@
 import y_config
 import y_packages
 import y_parse
+import os
 
 y_config.parse_c()
 y_packages.parse_p()
 
-import os
 for file in os.listdir("/"):
     if file.endswith(".y"):
         y_parse.parse_c(file)
