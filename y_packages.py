fac8911c686aabd6ff090c0087cdefafbb5ac012
Zack P <zack@zacklearns.com>
2017-07-28 00:16:56 -0400

    Create y_packages.py

---
+++ y_packages.py
@@ -0,0 +1,41 @@
+from pyparsing import *
+
+def parse_y():
+  data = open("conf.y","r")
+  
+  
+  LBRACE,RBRACE,LPAREN,RPAREN,SEMI,COL,AT,DOT = map(Suppress,"{}();:@.")
+  
+  
+  real = Regex(r"[+-]?\d+\.\d*").setParseAction(lambda t:float(t[0]))
+  integer = Regex(r"[+-]?\d+").setParseAction(lambda t:int(t[0]))
+  
+  
+  string = QuotedString('"')
+  
+
+  PACK = Keyword("Package")
+  
+  
+  DATA = Keyword("Data")
+
+  
+  TREE = Keyword("tree")
+  
+  
+  tree = Group(TREE + LPAREN + AT + string("project") + LPAREN + LBRACE + Group(ZeroOrMore(pack | data))("body") + RBRACE)
+  
+  
+  pack = Group(PACK + DOT + string("package"))
+  
+  
+  data = Group(DATA + DOT + string("data"))
+  
+  
+  tree.ignore(cStyleComment)
+  
+  
+  result = tree.parseString(data)
+  
+  
+  print(result.asList())
