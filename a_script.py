04811239f5defa0f5be6bb157b54c226a8a60dab
Zack P <zack@zacklearns.com>
2017-07-28 08:54:14 -0600

    Update a_script.py

--- a_script.py
+++ a_script.py
@@ -1,5 +1,13 @@
 import y_config
 import y_packages
+import y_parse
 
 y_config.parse_c()
 y_packages.parse_p()
+
+import os
+for file in os.listdir("/"):
+    if file.endswith(".y"):
+        y_parse.parse_c(file)
+        
+print("Parse all Yabbit files.")
