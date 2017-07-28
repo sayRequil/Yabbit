7c3c905876a368d358c1aaddb28fc449d6f911f8
Zack P <zack@zacklearns.com>
2017-07-28 08:50:15 -0600

    Update y_parse.py

--- y_parse.py
+++ y_parse.py
@@ -1,7 +1,7 @@
 from pyparsing import *
 
-def parse_c():
-  data = open("conf.y","r")
+def parse_c(file):
+  data = open(file,"r")
   
   
   LBRACE,RBRACE,LPAREN,RPAREN,SEMI,COL,AT = map(Suppress,"{}();:@")
@@ -25,15 +25,29 @@ def parse_c():
   VERSION = Keyword("version")
   LANGUAGE = Keyword("language")
   PACKAGE = Keyword("package")
+  EMAIL = Keyword("email")
   
   
+  # Other
+  ENTRY = Keyword("entry")
+  
+  
+  # Version
+  YABBIT = Keyword("Yabbit")
+  
+  
+  # Defining syntax
   file = Group(SETTINGS + DOT + FILE + COL + string("file") + SEMI)
   author = Group(SETTINGS + DOT + AUTHOR + COL + string("author") + SEMI)
-  version = Group(SETTINGS + DOT + VERSION + COL + string("version" + SEMI))
-  language = Group(SETTINGS + DOT + LANGUAGE + COL + string("language"))
+  version = Group(SETTINGS + DOT + VERSION + COL + string("version") + SEMI)
+  language = Group(SETTINGS + DOT + LANGUAGE + COL + string("language") + SEMI)
+  package = Group(SETTINGS + DOT + PACKAGE + COL + string("package") + SEMI)
+  email = Group(SETTINGS + DOT + EMAIL + COL + string("email") + SEMI)
+  yabbit = Group(YABBIT + DOT + real + COL + string("yabbit") + SEMI) # Yabbit.1.0.0:"latest"
+  entry = Group(ENTRY + COL + string("entry") + SEMI)
   
   
-  tree = Group(TREE + LPAREN + AT + string("project") + LPAREN + LBRACE + Group(ZeroOrMore(file | author | version | language))("body") + RBRACE)
+  tree = Group(TREE + LPAREN + AT + string("project") + LPAREN + LBRACE + Group(ZeroOrMore(file | author | version | language | package | email | yabbit))("body") + RBRACE)
   
   
   tree.ignore(cStyleComment)
