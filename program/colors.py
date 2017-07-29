1aa25b00e8f41ea05d1d499f077f3d158a022ada
Zack P <zack@zacklearns.com>
2017-07-28 18:13:22 -0600

    Delete colors.py

--- program/colors.py
+++
@@ -1,6 +0,0 @@
-def colorss(color):
-  if color == "preset": # (The functions that came with, like Entry)
-    return "blue"
-    
-  if color == "string": # (A string that has quotes, like this: "Hello World")
-    return "purple"
